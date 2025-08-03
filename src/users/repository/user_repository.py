7ad885ea37706a5c09e52405676a40cb851ddf16
Max Meza <matt1007k@gmail.com>
2025-08-02 22:47:06 -0500

    Add get one by id in user repo

--- src/users/repository/user_repository.py
+++ src/users/repository/user_repository.py
@@ -13,6 +13,13 @@ class UserRepository:
         self.db.commit()
         return user
 
+    def get_one_by_id(self, id: str) -> Optional[User]:
+        try:
+            return self.db.query(User).get(id)
+        except Exception as e:
+            print(f"Error get_one_by_id: {e}")
+            return None
+
     def get_one_by_email(self, email: str) -> Optional[User]:
         try:
             return self.db.query(User).filter(User.email == email).first()
